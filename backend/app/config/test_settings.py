2db1476d4c29af32a044ab6e80afea218859b3f0
Mark-Mekhail <markm2hs@gmail.com>
2025-05-21 20:51:35 -0700

    chore: remove test settings configuration file

--- backend/app/config/test_settings.py
+++
@@ -1,7 +0,0 @@
-from app.config import settings
-
-# Override settings for testing environment
-settings.ENVIRONMENT = "test"
-
-# Add mock model settings for tests that don't need a real model
-settings.MOCK_MODEL_IN_TESTS = True
